92affdb953938642e5e3810f9a2ad299d4f8adb5
Jesse Dillman <jesse@thinkboxcreative.com>
2018-06-16 01:05:34 -0500

    add algolia search

--- search.php
+++ search.php
@@ -1,17 +1,16 @@
 <?php
 
+// Only doing this because webtasks won't let me authenticate :(
+// https://github.com/auth0/wt-cli/issues/212
+
 $terms = urlencode($_GET['s']);
-$app_id = 'Y2N7OVVUWU';
-$app_key = '7367f7a29848e86610c68b9f09b51dfd';
+$app_id = 'VP222X0ZZ6';
+$app_key = 'd3909cbd70bb4a2fafdd72e9d3bd37e5';
 
 $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, 'https://'.$app_id.'-dsn.algolia.net/1/indexes/forestry?query='.$terms.'&hitsPerPage=15&getRankingInfo=1');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true );
-
-curl_setopt($curl, CURLOPT_VERBOSE, true);
-$verbose = fopen('test.txt', 'w+');
-curl_setopt($curl, CURLOPT_STDERR, $verbose);
 
 $headers =  array(
 	'X-Algolia-Application-Id: '.$app_id,
